ccf1a6a03cd979abc211733e9edad29e15527834
G7techglob <globalinfortv24@gmail.com>
2026-05-25 05:41:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const menuBtn = document.getElementById("menu-btn");
 const navbar = document.getElementById("navbar");
 
-menuBtn.addEventListener("click", () => {
+/* OPEN / CLOSE MENU */
+menuBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
   navbar.classList.toggle("active");
 });
 
-// CLOSE WHEN CLICK LINK (RETURN TO DASHBOARD SECTION)
+/* CLOSE ON LINK CLICK (RETURN TO DASHBOARD) */
 document.querySelectorAll(".navbar a").forEach(link => {
   link.addEventListener("click", () => {
     navbar.classList.remove("active");
   });
 });
 
-// CLOSE WHEN CLICK OUTSIDE
+/* CLOSE WHEN CLICK OUTSIDE */
 document.addEventListener("click", (e) => {
   if(!navbar.contains(e.target) && !menuBtn.contains(e.target)){
     navbar.classList.remove("active");
