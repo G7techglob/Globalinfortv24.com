f66666726bd3e9ead034551e38bdaff3aae2a657
G7techglob <globalinfortv24@gmail.com>
2026-05-26 01:17:39 +0200

    Create about.js

---
+++ about.js
@@ -0,0 +1,8 @@
+// about.js
+
+const menuBtn = document.getElementById("menuBtn");
+const navbar = document.getElementById("navbar");
+
+menuBtn.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
