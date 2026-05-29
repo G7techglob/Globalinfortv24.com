1c5d0d99a7672a2a5919663050a04ce98c653b93
G7techglob <globalinfortv24@gmail.com>
2026-05-29 17:55:37 +0200

    Update business.js

--- business.js
+++ business.js
@@ -18,3 +18,78 @@ document.querySelector(".newsletter-form").addEventListener("submit", function(e
     alert("Please enter a valid email address.");
   }
 });
+
+const rssContainer = document.getElementById("rss-feed");
+
+const feeds = [
+
+"https://feeds.bbci.co.uk/news/business/rss.xml",
+
+"https://feeds.reuters.com/reuters/businessNews",
+
+"https://www.cnbc.com/id/100003114/device/rss/rss.html",
+
+"https://finance.yahoo.com/news/rssindex",
+
+"https://www.forbes.com/business/feed/",
+
+"https://www.france24.com/en/business-tech/rss",
+
+"https://rss.nytimes.com/services/xml/rss/nyt/Business.xml",
+
+"https://www.aljazeera.com/xml/rss/all.xml",
+
+"https://feeds.a.dj.com/rss/RSSMarketsMain.xml"
+
+];
+
+feeds.forEach(feed => {
+
+const rssUrl =
+`https://api.rss2json.com/v1/api.json?rss_url=${feed}`;
+
+fetch(rssUrl)
+
+.then(res => res.json())
+
+.then(data => {
+
+data.items.slice(0, 4).forEach(item => {
+
+const card = document.createElement("div");
+
+card.classList.add("rss-card");
+
+card.innerHTML = `
+
+<img src="${item.thumbnail || 'https://via.placeholder.com/400'}">
+
+<div class="rss-content">
+
+<h3>${item.title}</h3>
+
+<p>
+
+${item.description.replace(/<[^>]+>/g, '').substring(0,120)}...
+
+</p>
+
+<a href="${item.link}" target="_blank">
+
+Read Full News →
+
+</a>
+
+</div>
+
+`;
+
+rssContainer.appendChild(card);
+
+});
+
+})
+
+.catch(error => console.log(error));
+
+});
